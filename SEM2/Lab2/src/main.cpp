6209501b89dd5da391281e3dfc580eb2102dd547
Schlypar <schlypar@gmail.com>
2023-03-25 23:40:03 +0300

    Preparing to add dependency

--- SEM2/Lab2/src/main.cpp
+++ SEM2/Lab2/src/main.cpp
@@ -1,7 +1,15 @@
+// #include "../imgui/imgui.h"
+#include "../imgui/imgui.h"
+#include "../imgui/imgui_impl_glfw.h"
+#include "../imgui/imgui_impl_opengl3.h"
+#include "../imgui/GL/glew.h"
+#include "../imgui/GLFW/glfw3.h"
+
 #include "ADT.h"
 #include "ArraySequence.h"
 #include "Sequence.h"
 #include "moduleTest.h"
+
 
 #define LINE "\n<==================================================>\n"
 
@@ -14,12 +22,84 @@ void Test()
 	TestSequence();
 }
 
-int main()
+int main(int, char**)
 {
-	Logger::setPriority(ErrorPriority);
-	Test();
+    // Initialize GLFW
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return 1;
+    }
 
-	Sequence<int>* seq = (Sequence<int>*) new ArraySequence<int>;
-	 
-	return 0;
+    // Decide GL+GLSL versions
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    // Create window with graphics context
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "ImGui Window", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    // Initialize GLEW
+    GLenum err = glewInit();
+    if (err != GLEW_OK)
+    {
+        fprintf(stderr, "Failed to initialize GLEW: %s\n", glewGetErrorString(err));
+        glfwTerminate();
+        return 1;
+    }
+
+    // Setup ImGui binding
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    // Setup style
+    ImGui::StyleColorsDark();
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Poll and handle events (inputs, window resize, etc.)
+        glfwPollEvents();
+
+        // Start the ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // Create a simple ImGui window
+        ImGui::Begin("Simple Window");
+        ImGui::Text("Hello, world!");
+        ImGui::End();
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
