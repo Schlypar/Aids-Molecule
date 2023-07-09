0ba9fddea08c7fad4549c067f94d178e649bd959
Schlypar <schlypar@gmail.com>
2023-07-09 14:39:52 +0300

    added docs for ListSequence

--- SEM2/Lab2-3-4/include/Sequence/ListSequence.h
+++ SEM2/Lab2-3-4/include/Sequence/ListSequence.h
@@ -3,6 +3,7 @@
 #include "Logger.h"
 #include "SegmentedList.h"
 #include "Sequence.h"
+#include <cwchar>
 
 template <typename T>
 class ListSequence : public Sequence<T>
@@ -174,61 +175,32 @@ public:
 		Logger::Info("Destroyed ListSequence<T>");
 	}
 
-	T& GetFirst() const override
-	{
-		if (isEmpty())
-		{
-			Logger::Trace("At Get() at ArraySequence.h");
-			logException(EXCEPTION_INDEX_OUT_OF_RANGE);
-			exit(EXIT_FAILURE);
-		}
+	// Gets an intance of what is at the head
+	T& GetFirst() const override;
 
-		return container.GetFirst();
-	}
+	// Gets an intance of what is at the tail
+	T& GetLast() const override;
 
-	T& GetLast() const override
-	{
-		if (isEmpty())
-		{
-			Logger::Trace("At Get() at ArraySequence.h");
-			logException(EXCEPTION_INDEX_OUT_OF_RANGE);
-			exit(EXIT_FAILURE);
-		}
+	// Gets an intance of what is at the index
+	T& Get(const Index index) const override;
 
-		return container.GetLast();
-	}
+	/*
+	 * Appends data to the sequence and returns pointer to itself
+	 * */
+	Sequence<T>* Append(const T& data) override;
 
-	T& Get(const Index index) const override
-	{
-		if (isEmpty())
-		{
-			Logger::Trace("At Get() at ArraySequence.h");
-			logException(EXCEPTION_INDEX_OUT_OF_RANGE);
-			exit(EXIT_FAILURE);
-		}
+	/*
+	 * Prepends data to the sequence and returns pointer to itself
+	 * */
+	Sequence<T>* Prepend(const T& data) override;
 
-		return container.Get(index);
-	}
-
-	Sequence<T>* Append(const T& data) override
-	{
-		container.Append(data);
-
-		return this;
-	}
-
-	Sequence<T>* Prepend(const T& data) override
-	{
-		container.Prepend(data);
-
-		return this;
-	}
-
+	// Inserts exactly at the index inputed
 	void InsertAt(const Index index, const T& data) override
 	{
 		container.insertAt(index, data);
 	}
 
+	// Removes exactly index inputed
 	void Remove(const Index index) override
 	{
 		for (Index i = index; i < GetLength() - 1; i++)
@@ -256,16 +228,19 @@ public:
 		return nullptr;
 	}
 
+	// Returns abstract class Sequence that underhood is ListSequence
 	Sequence<T>* Create() const override
 	{
 		return (Sequence<T>*) new ListSequence<T>();
 	}
 
+	// Returns abstract class Sequence that underhood is ListSequence and copies data to it
 	Sequence<T>* Copy() const override
 	{
 		return (Sequence<T>*) new ListSequence<T>(*this);
 	}
 
+	// Gets an intance of what is at the index
 	T& operator[](const Index index)
 	{
 		return container[index];
@@ -313,3 +288,59 @@ public:
 		return stream;
 	}
 };
+
+template <typename T>
+T& ListSequence<T>::GetFirst() const
+{
+	if (isEmpty())
+	{
+		Logger::Trace("At Get() at ArraySequence.h");
+		logException(EXCEPTION_INDEX_OUT_OF_RANGE);
+		exit(EXIT_FAILURE);
+	}
+
+	return container.GetFirst();
+}
+
+template <typename T>
+T& ListSequence<T>::GetLast() const
+{
+	if (isEmpty())
+	{
+		Logger::Trace("At Get() at ArraySequence.h");
+		logException(EXCEPTION_INDEX_OUT_OF_RANGE);
+		exit(EXIT_FAILURE);
+	}
+
+	return container.GetLast();
+}
+
+template <typename T>
+T& ListSequence<T>::Get(const Index index) const
+{
+	if (isEmpty())
+	{
+		Logger::Trace("At Get() at ArraySequence.h");
+		logException(EXCEPTION_INDEX_OUT_OF_RANGE);
+		exit(EXIT_FAILURE);
+	}
+
+	return container.Get(index);
+}
+
+template <typename T>
+Sequence<T>* ListSequence<T>::Append(const T& data)
+{
+	container.Append(data);
+
+	return this;
+}
+
+template <typename T>
+
+Sequence<T>* ListSequence<T>::Prepend(const T& data)
+{
+	container.Prepend(data);
+
+	return this;
+}
